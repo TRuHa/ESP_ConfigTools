902868ab3a20dc1b24c2d23b16bb19879b6bfd72
TRuHa <esetruha@gmail.com>
2018-10-31 21:19:52 +0100

    Actualización y depuración.

--- web/server.py
+++ web/server.py
@@ -1,24 +1,29 @@
-def show_web(html):
-    header = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode()
-    with open("page/" + html, "rb") as file:
-        body = header + file.read()
+import machine
+import socket
+import os
 
-    return header, body
+
+def show_web(html):
+    with open("web/" + html, "rb") as file:
+        body = file.read()
+
+    return body
 
 
 def save_data(web, items):
-    import ujson
     if web == 'network.html':
         with open("config.json", "r+") as outfile:
             data = ujson.load(outfile)
             subdata = data['network']
-
             for item in subdata:
                 item["method"] = items[0].lstrip('method=')
                 item["ip"] = items[1].lstrip('ip=')
                 item["mask"] = items[2].lstrip('mask=')
                 item["gate"] = items[3].lstrip('gate=')
                 item["dns"] = items[3].lstrip('gate=')
+            subdata = data['config']
+            for item in subdata:
+                item['network'] = 'YES'
 
             outfile.seek(0)
             outfile.write(ujson.dumps(data))
@@ -36,6 +41,54 @@ def save_data(web, items):
             for item in subdata:
                 item['essid'] = essid
                 item['psk'] = items[1].lstrip('psk=')
+            subdata = data['config']
+            for item in subdata:
+                item['wifi'] = 'YES'
+
+            outfile.seek(0)
+            outfile.write(ujson.dumps(data))
+            outfile.truncate()
+
+    elif web == 'mqtt.html':
+        topic = items[5].lstrip('topic=')
+        if '%2F' in topic:
+            topic = topic.replace("%2F", "/")
+
+        with open("config.json", "r+") as outfile:
+            data = ujson.load(outfile)
+            subdata = data['mqtt']
+
+            for item in subdata:
+                item['id'] = items[0].lstrip('id=')
+                item['broker'] = items[1].lstrip('broker=')
+                item['port'] = items[2].lstrip('port=')
+                item['user'] = items[3].lstrip('user=')
+                item['psw'] = items[4].lstrip('psw=')
+                item['topic'] = topic
+            subdata = data['config']
+            for item in subdata:
+                item['mqtt'] = 'YES'
+
+            outfile.seek(0)
+            outfile.write(ujson.dumps(data))
+            outfile.truncate()
+
+    elif web == 'mode.html':
+        type = items[1].lstrip('type=')
+        if '+' in type:
+            type = type.replace("+", " ")
+
+        with open("config.json", "r+") as outfile:
+            data = ujson.load(outfile)
+            subdata = data['mode']
+
+            for item in subdata:
+                item['model'] = items[0].lstrip('model=')
+                item['type'] = type
+                item['gpio'] = items[2].lstrip('gpio=')
+            subdata = data['config']
+            for item in subdata:
+                item['mode'] = 'YES'
 
             outfile.seek(0)
             outfile.write(ujson.dumps(data))
@@ -82,10 +135,6 @@ def save_data(web, items):
 
 
 def run():
-    import machine
-    import socket
-    import os
-
     addr = socket.getaddrinfo('0.0.0.0', 80)[0][- 1]
 
     sock = socket.socket()
@@ -100,7 +149,7 @@ def run():
     print('[|] Esperando cliente...')
 
     clients = []
-    webs = ['network.html', 'wifi.html', 'mqtt.html', 'mode.html']
+    webs = ['network.html', 'wifi.html', 'mqtt.html', 'mode.html', 'end']
 
     while True:
         client, addr = sock.accept()
@@ -109,10 +158,10 @@ def run():
             clients.append(addr[0])
             print('[+] Cliente %s conectado.' % addr[0])
 
-        msg1 = client.recv(1500).decode()
-        msg1 = msg1.split('\n')
+        msg = client.recv(1500).decode()
+        msg = msg.split('\n')
 
-        extract = msg1[0]
+        extract = msg[0]
         extract = extract.split(' ')
         method = extract[0]
 
@@ -124,54 +173,48 @@ def run():
             html = 'network.html'
 
         if method == 'GET':
-            if 'index' in html:
-                try:
-                    html = webs[last + 1]
+            if '?' in html:
+                html = html.rstrip('?')
 
-                except IndexError:
-                    print('[-] Sistema configurado correctamente, reiniciando...')
-                    header = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode()
-                    body = '<center><h4>Configuration saved successfully.</h4><p>Rebooting the ' \
-                           'system.</p></center>'.encode()
+            if html == 'end':
+                print('[-] Sistema configurado correctamente, reiniciando...')
+                body = '<html><center><h4>Configuration saved successfully.</h4><p>Rebooting the ' \
+                       'system.</p></center></html>'.encode()
 
-                    response = header + body
+                client.send(body)
+                client.close()
+                sock.close()
 
-                    client.send(response)
-                    client.close()
-                    sock.close()
+                machine.reset()
 
-                    machine.reset()
+                break
 
-                    break
-
-            if html in webs:
-                header, body = show_web(html)
+            elif html in webs:
+                body = show_web(html)
                 last = webs.index(html)
 
             else:
                 print('[-] Error: pagina no encontrada')
-                header = 'HTTP/1.1 404 Not Found\n\n'.encode()
-                body = '<center><h4>Error 404: File not found</h4><p>Python HTTP Server</p></center>'.encode()
+                body = '<html><center><h4>Error 404: File not found</h4><p>Python HTTP '\
+                       'Server</p></center></html>'.encode()
 
         elif method == 'POST':
-            msg2 = client.recv(1500).decode()
-            msg2 = msg2.split('\n')
+            msg = client.recv(1500).decode()
+            msg = msg.split('\n')
 
-            count = len(msg2)
-            answer = msg2[count - 1]
+            count = len(msg)
+            answer = msg[count - 1]
             answer = answer.split('&')
 
             save_data(webs[last], answer)
 
             print('[-] Datos %s guardados.' % webs[last])
 
-            header = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'.encode()
-            body = '<center><h4>Configuration saved successfully.</h4><form method="get" ' \
-                   'action="next.html"><button type="submit">Next</button></form></center>'.encode()
+            body = '<html><center><h4>Configuration saved successfully.</h4><form method="get" ' \
+                   'action=%s><button type="submit">Next</button></form></center></html>' % (webs[last + 1])
+            body = body.encode()
 
-        response = header + body
-
-        client.send(response)
+        client.send(body)
         client.close()
 
 
